b4246d5b0056c248af78dfb25d353ec9549ca31f
Zac W <zacwalters4@gmail.com>
2023-10-03 21:30:44 -0600

    Create data set

---
+++ src/Data/Data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        transactionTotal: 125,
+        date: "10/3/2023"
+    },
+    {
+        transactionTotal: 10,
+        date: "10/1/2023"
+    },
+    {
+        transactionTotal: 76,
+        date: "9/25/2023"
+    },
+    {
+        transactionTotal: 24,
+        date: "9/19/2023"
+    },
+    {
+        transactionTotal: 167,
+        date: "9/19/2023"
+    },
+    {
+        transactionTotal: 255,
+        date: "9/10/2023"
+    },
+    {
+        transactionTotal: 47,
+        date: "9/10/2023"
+    },
+    {
+        transactionTotal: 93,
+        date: "9/1/2023"
+    },
+    {
+        transactionTotal: 67,
+        date: "8/26/2023"
+    },
+    {
+        transactionTotal: 144,
+        date: "8/17/2023"
+    }   
+] 
